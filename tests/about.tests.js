47f50eb1cfb0259e333adfd88490b4e86e25b97f
krasen84 <116521221+krasen84@users.noreply.github.com>
2024-02-21 01:23:42 +0200

    update fix 3

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -5,7 +5,7 @@ suite('About page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/about");
     let body = await res.text();
-    assert.ok(body.includes("<title>Hello Dev Ops</title>"));
-    assert.ok(body.includes("<h1>About</h1>"));
+    assert.ok(body.includes("<title>About</title>"));
+    assert.ok(body.includes("<h1>Hello Dev Ops</h1>"));
   });
 });
